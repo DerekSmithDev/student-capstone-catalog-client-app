2d5195f9bfbdd4972257e6026718c0289064cd79
mikeevans85 <baudecordier@gmail.com>
2018-12-03 21:22:51 -0600

    create show page and update routes;

---
+++ src/views/Show.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Home">
+    <div class="container">
+      <h1>{{ capstone.name }}</h1>
+      <div class="images"><img v-bind:src="capstone.images[0]" alt="" /></div>
+      <h2>{{ capstone.url }}</h2>
+      <p>{{ capstone.description }}</p>
+      <a href="/#/">Back to all capstone</a>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+var axios = require("axios");
+export default {
+  data: function() {
+    return {
+      capstone: {}
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/students/capstones/" + this.$route.params.id).then(
+      function(response) {
+        this.capstone = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
+</script>
